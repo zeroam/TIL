90f793dc4fe78e5133304524dffc6872db46f13b
jone <imdff0803@gmail.com>
2019-05-30 17:15:57 +0900

    django celery - docker-compose setting

--- django_tutorial/image_parroter/image_parroter/settings.py
+++ django_tutorial/image_parroter/image_parroter/settings.py
@@ -131,8 +131,8 @@ if not os.path.exists(MEDIA_ROOT) or not os.path.exists(IMAGES_DIR):
 
 
 # celery
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
