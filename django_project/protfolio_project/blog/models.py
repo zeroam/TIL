aa5f1fba5b1888e855b2f3b228e9d07572ab665b
jone <imdff0803@gmail.com>
2019-08-09 14:33:44 +0900

    Added bootstrap and jobs are showing up

--- django_project/protfolio_project/blog/models.py
+++ django_project/protfolio_project/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+# Create A Blog models
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+
+# Add the Blog app to the setting
+
+# Create a migration
+
+# Migrate
+
+# Add to the main
--- django_project/protfolio_project/portfolio/settings.py
+++ django_project/protfolio_project/portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
