9c4022f12e1a25a885308267c2bf829a5ddf16e3
jone <imdff0803@naver.com>
2019-10-31 02:18:45 +0900

    python pyqt5

---
+++ python/pyqt/README.md
@@ -0,0 +1,1 @@
+- [Python and PyQt: Building a GUI Desktop Calculator](https://realpython.com/python-pyqt-gui-calculator/)
---
+++ python/pyqt/hello.py
@@ -0,0 +1,26 @@
+""" Simple Hello World example with PyQt5 """
+import sys
+
+# 1. Import 'QApplication' and all the required widgets
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+
+
+# 2. Create an instance of QApplication
+# app = QApplication([])
+app = QApplication(sys.argv)
+
+# 3. Create an instance of your application's GUI
+window = QWidget()
+window.setWindowTitle('PyQt App')
+window.setGeometry(100, 100, 280, 80)
+window.move(60, 15)
+helloMsg = QLabel('<h1>Hello World</h1>', parent=window)
+helloMsg.move(50, 20)
+
+# 4. Show your application's GUI
+window.show()
+
+# 5. Run your application's event loop (or main loop)
+sys.exit(app.exec_())
