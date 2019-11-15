2c8612e0f985488b0015605b03e051e8dccffc28
jone <imdff0803@naver.com>
2019-11-15 09:32:34 +0900

    pyqt tutorial

---
+++ python/pyqt/LearnPyQt/start.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
+
+
+# Subclass QMainWindow to customise your applicatin's main window
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Jayone's Awesome App")
+
+        label = QLabel('This is a PyQt5 window!')
+
+        # The 'Qt' namespace has a lot of attributes to customise
+        # widgets. See: http://doc.qt.io/qt-5/qt.html
+        label.setAlignment(Qt.AlignCenter)
+
+        # Set the central widget of the Window.
+        # Widget will expand to take up all the space
+        # in the window by default
+        self.setCentralWidget(label)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+# start the event loop
+app.exec_()
