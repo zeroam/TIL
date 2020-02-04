642b56f6338336a953ce32fc556f636b08049ee6
jayone <imdff0803@gmail.com>
2020-02-05 07:51:27 +0900

    django project

--- django_project/image_parroter/web.sh
+++ django_project/image_parroter/web.sh
@@ -6,4 +6,4 @@ until psql -h "db" -U "postgres" -c '\q'; do
 done
 >&2 echo "Postgres is up - executing command"
 
-python manage.py migrate --noinput && python manage.py runserver 0.0.0.0:8000
+python manage.py migrate --noinput && python manage.py makemigrations && python manage.py runserver 0.0.0.0:8000
