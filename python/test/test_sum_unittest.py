1e8424d17f1386aab770e8d3f39abca8c1199eb7
jayone <imdff0803@gmail.com>
2020-02-21 10:30:10 +0900

    python test

--- python/test/test_sum_unittest.py
+++ python/test/test_sum_unittest.py
@@ -9,6 +9,11 @@ class TestSum(unittest.TestCase):
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 6, 'Should be 6')
 
+    def test_bad_type(self):
+        data = 'banana'
+        with self.assertRaises(TypeError):
+            result = sum(data)
+
 
 if __name__ == '__main__':
     unittest.main()
