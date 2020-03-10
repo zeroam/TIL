9d35f3e96800829d99e38573a741a8cf1c7779a0
jayone <imdff0803@gmail.com>
2020-03-10 19:54:01 +0900

    CleanCode chapter7 - handling exceptions

---
+++ books/CleanCode/ch7/130p.java
@@ -0,0 +1,52 @@
+// DeviceController.java
+public class DeviceController {
+    ...
+    public void sendShutDown() {
+        DeviceHandle handle = getHandle(DEV1);
+        // 디바이스 상태를 점검한다.
+        if (handle != DeviceHandle.INVALID) {
+            // 레코드 필드에 디바이스 상태를 저장한다.
+            retrieveDeviceRecord(handle);
+            // 디바이스가 일시정지 상태가 아니라면 종료한다.
+            if (record.getStatus() != DEVICE_SUSPENDED) {
+                pauseDevice(handle);
+                clearDeviceWorkQueue(handle);
+                closeDevice(handle);
+            } else {
+                logger.log("Device suspended. Unable to shut down");
+            }
+        } else {
+            logger.log("Invalid handle for: " + DEV1.toString());
+        }
+    }
+    ...
+}
+
+
+// DeviceController.java (예외사용) -> 오류가 발생하면 예외를 던지는 편이 나음
+public class DeviceController {
+    ...
+    public void sendShutDown() {
+        try {
+            tryToShutDown();
+        } catch (DeviceShutDownError e) {
+            logger.log(e);
+        }
+    }
+
+    private void tryToShutDown() throws DeviceShutDownError {
+        DeviceHandle handle = getHandle(DEV1);
+        DeviceRecord record = retrieveDeviceRecord(handle);
+
+        pauseDevice(handle);
+        clearDeviceWorkQueue(handle);
+        closeDevice(handle);
+    }
+
+    private DeviceHandle getHandle(DeviceID id) {
+        ...
+        throw new DeviceShutDownError("Invalid handle for: " + id.toString());
+        ...
+    }
+    ...
+}
