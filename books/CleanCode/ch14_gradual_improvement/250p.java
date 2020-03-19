1294e38111999aff55ca8ef11a917f6126aa4b76
jayone <imdff0803@gmail.com>
2020-03-19 20:40:28 +0900

    CleanCode chapter14 - Args implement

---
+++ books/CleanCode/ch14_gradual_improvement/250p.java
@@ -0,0 +1,71 @@
+// ArgumentMarshaler.java
+public interface ArgumentMarshaler {
+    void set(Iterator<String> currentArgument) throws ArgsException;
+}
+
+
+// BooleanArgumentMarshaler.java
+public class BooleanArgumentMarshaler implements ArgumentMarshaler {
+    private boolean booleanValue = false;
+
+    public void set(Iterator<String> currentArgument) throws ArgsException {
+        booleanValue = true;
+    }
+
+    public static boolean getValue(ArgumentMarshaler am) {
+        if (am != null && am instanceof BooleanArgumentMarshaler)
+            return ((BooleanArgumentMarshaler) am).booleanValue;
+        else
+            return false;
+    }
+}
+
+
+// StringArgumentMarshaler.java
+import static com.objectmentor.utilities.args.ArgsException.ErrorCode.*;
+
+public class StringArgumentMarshaler implements ArgumentMarshaler {
+    private String stringValue = "";
+
+    public void set(Iterator<String> currentArgument) throws ArgsException {
+        try {
+            stringValue = currentArgument.next();
+        } catch (NoSuchElementException e) {
+            throw new ArgsException(MISSING_STRING);
+        }
+    }
+
+    public static String getValue(ArgumentMarshaler am) {
+        if (am != null && am instanceof StringArgumentMarshaler)
+            return ((StringArgumentMarshaler) am).stringValue;
+        else
+            return "";
+    }
+}
+
+
+// IntegerArgumentMarshaler.java
+import static com.objectmentor.utilities.args.ArgsException.ErrorCode.*;
+
+public class IntegerArgumentMarshaler implements ArgumentMarshaler {
+    private int intValue = 0;
+
+    public void set(Iterator<String> currentArgument) throws ArgsException {
+        String parameter = null;
+        try {
+            parameter = currentArgument.next();
+            intValue = Integer.parseInt(parameter);
+        } catch (NoSuchElementException e) {
+            throw new ArgsException(MISSING_INTEGER);
+        } catch( NumberFormatException e) {
+            throw new ArgsException(INVALID_INTEGER, parameter);
+        }
+    }
+
+    public static int getValue(ArgumentMarshaler am) {
+        if (am != null && am instanceof IntegerArgumentMarshaler)
+            return ((IntegerArgumentMarshaler) am).intValue;
+        else
+            return 0;
+    }
+}
