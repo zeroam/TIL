f7ec81de7f3fdcb194d620e96cbcd0f6b81cdf39
jayone <imdff0803@gmail.com>
2020-04-17 21:04:07 +0900

    Clean Code in Python - Chapter 9: Common Design Patterns

---
+++ books/PythonCleanCode/ch9_design_pattern/test_decorator_1.py
@@ -0,0 +1,42 @@
+"""Clean Code in Python - Chapter 9: Common Design Patterns
+
+> Test Decorator
+"""
+import unittest
+
+from decorator_1 import CaseInsensitive, DictQuery, RemoveEmpty
+
+
+class TestDecoration(unittest.TestCase):
+    def setUp(self):
+        self.query = DictQuery(
+            foo="bar", empty="", none=None, upper="UPPERCASE", title="Title"
+        )
+
+    def test_no_decorate(self):
+        expected = {
+            "foo": "bar",
+            "empty": "",
+            "none": None,
+            "upper": "UPPERCASE",
+            "title": "Title",
+        }
+        self.assertDictEqual(self.query.render(), expected)
+
+    def test_decorate_remove_empty(self):
+        expected = {
+            "foo": "bar",
+            "upper": "UPPERCASE",
+            "title": "Title",
+        }
+        result = RemoveEmpty(self.query).render()
+        self.assertEqual(result, expected)
+
+    def test_decorator(self):
+        expected = {"foo": "bar", "upper": "uppercase", "title": "title"}
+        result = CaseInsensitive(RemoveEmpty(self.query)).render()
+        self.assertDictEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
