280ed6578ded368c0082e13938acf51a0e961a4c
jayone <imdff0803@gmail.com>
2020-04-29 16:14:29 +0900

    nextjs for react serverside - tutorial

---
+++ webapp/hello-next/pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
+
+class MyDocument extends Document {
+    static getInitialProps({ renderPage }) {
+        const { html, head } = renderPage();
+        const styles = flush();
+
+        return { html, head, styles };
+    }
+
+    render() {
+        return (
+            <html>
+                <Head>
+                    <style>{`body { margin: 0 } /* custom! */`}</style>
+                    <title>Next.js 연습</title>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    };
+};
+
+export default MyDocument
