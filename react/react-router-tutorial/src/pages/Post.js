1512447d0bbe78b7f38b1d01b1d0b7d3c455358b
jayone <imdff0803@gmail.com>
2020-06-08 00:44:35 +0900

    react: react router tutorial

---
+++ react/react-router-tutorial/src/pages/Post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Post = ({ match }) => {
+  console.log('Post', match)
+
+  return (
+    <p>
+      포스트 #{match.params.id}
+    </p>
+  )
+}
+
+export default Post
