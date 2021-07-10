43e3573be44c7f7aa4600ff8a34e99a65cbdc5d1
jayone <imdff0803@gmail.com>
2021-07-10 15:47:17 +0900

    python logging: add custom handler

--- python/logging/realpython/file_logger.py
+++ python/logging/realpython/file_logger.py
@@ -1,15 +1,20 @@
 import logging
+import logging.handlers
 import logging.config
 from logging import StreamHandler
 
 
 class CustomStreamHandler(StreamHandler):
     def __init__(self, stream=None):
-        super().__init__(self, stream)
+        super().__init__(stream)
 
     def emit(self, record):
         msg = self.format(record)
         print(f"Custom: {msg}")
+
+
+# add to logging.handlers
+logging.handlers.CustomStreamHandler = CustomStreamHandler
 
 
 logging.config.fileConfig(fname="file.conf", disable_existing_loggers=False)
